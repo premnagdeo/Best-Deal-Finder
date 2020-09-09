6048cdb5a4b1bdc1c50042f8a8e854ce057663c6
premnagdeo <premnagdeo@gmail.com>
2020-09-09 16:53:07 +0530

    MDComputers Scraping Complete

--- scraper.py
+++ scraper.py
@@ -5,11 +5,9 @@ import time
 from collections import defaultdict
 
 
-
-
 class Scraper:
 
-    def __init__(self):
+    def __init__(self, product):
         # Initialize Options to start Chrome as headless in selenium
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.add_argument("--headless")
@@ -23,12 +21,16 @@ class Scraper:
         # To initialize the webdriver in a new window for Debugging:
         self.browser = webdriver.Chrome('chromedriver.exe')
 
-    def search(self, product):
         self.product = product
+        self.total_products_count = 8
+
+    def search(self):
         amazon_products_data = self.search_amazon()
         print(amazon_products_data)
         flipkart_products_data = self.search_flipkart()
         print(flipkart_products_data)
+        mdcomputers_products_data = self.search_mdcomputers()
+        print(mdcomputers_products_data)
 
     def search_amazon(self):
         self.browser.get('https://www.amazon.in/')
@@ -45,10 +47,10 @@ class Scraper:
 
         amazon_products_data = defaultdict(dict)
 
-        # Collect data of top 5 items:
+
         count = 0
         i = 0
-        while count < 5:
+        while count < self.total_products_count:
             item_div = main_div.find('div', {'data-index': str(i)})
 
             # Check if item is sponsored, skip it
@@ -78,7 +80,9 @@ class Scraper:
             # Get the price
             item_price = item_div.find('span', {'class': 'a-price-whole'})
             if item_price is not None:
-                amazon_products_data[count]['item_price'] = item_price.get_text().strip()
+                # Get rid of commas in the string
+                item_price = item_price.get_text().strip().replace(',', '')
+                amazon_products_data[count]['item_price'] = item_price
             else:
                 amazon_products_data[count]['item_price'] = 'Unavailable'
 
@@ -95,6 +99,7 @@ class Scraper:
 
         self.browser.get('https://www.flipkart.com/')
 
+        time.sleep(3)
         # Skip login prompt if it pops up:
         try:
             close_login_prompt_button = self.browser.find_element_by_xpath("/html/body/div[2]/div/div/button")
@@ -114,10 +119,9 @@ class Scraper:
 
         items_div = soup.find_all('div', {'class': '_3O0U0u'})
 
-
         flipkart_products_data = defaultdict(dict)
 
-        for count in range(min(5, len(items_div))):
+        for count in range(min(self.total_products_count, len(items_div))):
 
             item_div = items_div[count]
 
@@ -127,7 +131,6 @@ class Scraper:
                 item_name = item_name[:-3]
             flipkart_products_data[count]['item_name'] = item_name
 
-
             # Get the rating
             item_rating = item_div.find('div', {'class': 'hGSR34'})
             if item_rating is not None:
@@ -135,12 +138,13 @@ class Scraper:
             else:
                 flipkart_products_data[count]['item_rating'] = 'Unavailable'
 
-
             # Get the price
             item_price = item_div.find('div', {'class': '_1vC4OE _2rQ-NK'})
             if item_price is not None:
                 # Remove the rupees symbol at the beginning
-                flipkart_products_data[count]['item_price'] = item_price.get_text().strip()[1:]
+                # Get rid of commas in the string
+                item_price = item_price.get_text().strip()[1:].replace(',', '')
+                flipkart_products_data[count]['item_price'] = item_price
             else:
                 flipkart_products_data[count]['item_price'] = 'Unavailable'
 
@@ -148,14 +152,67 @@ class Scraper:
             item_link = item_div.find('a', {'class': '_31qSD5'})
             flipkart_products_data[count]['item_link'] = 'https://www.flipkart.com' + item_link['href']
 
-
         return flipkart_products_data
 
+    def search_mdcomputers(self):
+
+        self.browser.get('https://mdcomputers.in/')
+
+
+        search_bar = self.browser.find_element_by_xpath('/html/body/div[1]/header/div[2]/div/div/div[2]/div[1]/div/form/div/input')
+
+        search_bar.send_keys(self.product)
+        search_bar.submit()
+        time.sleep(5)
+
+        src = self.browser.page_source
+        soup = BeautifulSoup(src, 'html.parser')
+
+        items_div = soup.find_all('div', {'class': 'product-item-container'})
+        # print(items_div)
+
+        mdcomputers_products_data = defaultdict(dict)
+
+        for count in range(min(self.total_products_count, len(items_div))):
+            item_div = items_div[count]
+
+            # Get the name
+            item_name = item_div.find('a')['title']
+            mdcomputers_products_data[count]['item_name'] = item_name.strip()
+
+            # Get the rating
+
+            # First we check if the product has 0 reviews, if no reviews -> rating = Unavailable
+            item_rating = item_div.find('a', {'class': 'rating-num'}).get_text().strip()
+
+            if '0 reviews' in item_rating:
+                mdcomputers_products_data[count]['item_rating'] = 'Unavailable'
+
+            else:
+                # Rating is present
+                # For rating we find the number of stars that are colored out of 5 stars
+                colored_stars = item_div.find_all('i', {'class': 'fa fa-star fa-stack-1x'})
+                item_rating = len(colored_stars)
+                # print('DEBUG', rating)
+
+                mdcomputers_products_data[count]['item_rating'] = str(item_rating) + '/5'
+
+            # Get the price
+            item_price = item_div.find('span', {'class': 'price-new'})
+            # Remove the rupees symbol at the beginning
+            # Get rid of commas in the string
+            item_price = item_price.get_text().strip()[1:].replace(',', '')
+            mdcomputers_products_data[count]['item_price'] = item_price
+
+            # Get the link
+            item_link = item_div.find('a')['href']
+            mdcomputers_products_data[count]['item_link'] = item_link.strip()
+
+        return mdcomputers_products_data
 
 t1 = time.time()
 
+scraper = Scraper('rtx 2060')
+scraper.search()
 
-scraper = Scraper()
-scraper.search('rtx 2060')
-
-print(time.time() - t1)
+print('Total Time taken to complete:', time.time() - t1)

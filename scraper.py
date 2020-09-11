f05d7b96e5959f91c4181e801abb59d808066bd3
premnagdeo <premnagdeo@gmail.com>
2020-09-11 18:37:34 +0530

    Fixed Amazon Scraping

--- scraper.py
+++ scraper.py
@@ -10,7 +10,6 @@ class Scraper:
 
     def __init__(self, product):
 
-
         self.product = product
         self.total_products_count = 5
 
@@ -39,10 +38,8 @@ class Scraper:
         print("Time to scrape Flipkart =", flipkart_end_time - flipkart_start_time)
         # print("Time to scrape MDComputers =", mdcomputers_end_time - mdcomputers_start_time)
 
-
-
     def search_amazon(self):
-        try:
+        # try:
 
             # Define headers for request
             user_agent_list = [
@@ -60,14 +57,12 @@ class Scraper:
             url = 'https://www.amazon.in/s?k=' + search_url
             response = requests.get(url, headers=headers)
 
-
             soup = BeautifulSoup(response.content, 'html.parser')
-            # main_div = soup.find_all('span', {'data-component-type': 's-product-image'})
+
             main_div = soup.find_all('span', {'cel_widget_id': 'MAIN-SEARCH_RESULTS'})
             retry_count = 0
 
             # If the results are not present, retry by sending a request again until we hit the retry limit
-
             if main_div is None:
 
                 retry_limit = 10
@@ -81,6 +76,8 @@ class Scraper:
 
                     retry_count += 1
 
+            print(retry_count, url)
+            # print(response.content)
 
             # If we still did not receive the results, return -1 as amazon not reachable
             if main_div is None:
@@ -89,20 +86,23 @@ class Scraper:
 
             amazon_products_data = defaultdict(dict)
             count = 0
-
+            # print(main_div)
             for item_div in main_div:
+
 
                 # Check if item is sponsored, skip it
                 sponsored_div = item_div.find('div', {'data-component-type': 'sp-sponsored-result'})
                 if sponsored_div is not None:
                     continue
 
-                item_name = item_div.find('span', {'class': 'a-size-medium a-color-base a-text-normal'})
+                # item_name = item_div.find('span', {'class': 'a-size-medium a-color-base a-text-normal'})
+                item_name = item_div.find('img')['alt']
+
                 # Check if item is not listed on the amazon page
                 if item_name is None:
                     continue
 
-                amazon_products_data[count]['item_name'] = item_name.get_text().strip()
+                amazon_products_data[count]['item_name'] = item_name.strip()
 
                 # Get the rating
                 item_rating = item_div.find('span', {'class': 'a-declarative'})
@@ -134,15 +134,16 @@ class Scraper:
             return amazon_products_data
 
 
-
-        except Exception as e:
-            # Could not fetch data from Amazon
-            return -1
+        #
+        # except Exception as e:
+        #     # Could not fetch data from Amazon
+        #     return -1
 
     def search_flipkart(self):
         try:
 
-            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}
 
             url_list = ['https://www.flipkart.com/search?q=']
             search_list = self.product.split()
@@ -152,9 +153,8 @@ class Scraper:
             url = "".join(url_list[:-1])
 
             response = requests.get(url, headers=headers)
-            # print(response.content)
+
             soup = BeautifulSoup(response.content, 'html.parser')
-            # print(soup.prettify())
 
             all_divs = soup.find_all('div', {'class': 'bhgxx2 col-12-12'})[2:-2]
 
@@ -164,13 +164,13 @@ class Scraper:
             regex = re.compile("^[a-zA-z]*")
             count = 0
             for div in all_divs:
-                # print(div)
+
                 for item_div in soup.find_all("div", {"data-id": regex}):
 
                     # Get the name
                     item_name = item_div.find('img')['alt'].strip()
-                    # if item_name[-3:] == '...':
-                    #     item_name = item_name[:-3]
+                    if item_name[-3:] == '...':
+                        item_name = item_name[:-3]
                     flipkart_products_data[count]['item_name'] = item_name
 
                     # Get the rating
@@ -222,62 +222,62 @@ class Scraper:
 
     def search_mdcomputers(self):
         # try:
-            self.browser.get('https://mdcomputers.in/')
+        self.browser.get('https://mdcomputers.in/')
 
-            search_bar = self.browser.find_element_by_xpath('/html/body/div[1]/header/div[2]/div/div/div[2]/div[1]/div/form/div/input')
+        search_bar = self.browser.find_element_by_xpath('/html/body/div[1]/header/div[2]/div/div/div[2]/div[1]/div/form/div/input')
 
-            search_bar.send_keys(self.product)
-            search_bar.submit()
-            time.sleep(5)
+        search_bar.send_keys(self.product)
+        search_bar.submit()
+        time.sleep(5)
 
-            src = self.browser.page_source
-            soup = BeautifulSoup(src, 'html.parser')
+        src = self.browser.page_source
+        soup = BeautifulSoup(src, 'html.parser')
 
-            items_div = soup.find_all('div', {'class': 'product-item-container'})
-            # print(items_div)
+        items_div = soup.find_all('div', {'class': 'product-item-container'})
+        # print(items_div)
 
-            mdcomputers_products_data = defaultdict(dict)
+        mdcomputers_products_data = defaultdict(dict)
 
-            loop_range = min(self.total_products_count, len(items_div))
-            for count in range(loop_range):
-                item_div = items_div[count]
+        loop_range = min(self.total_products_count, len(items_div))
+        for count in range(loop_range):
+            item_div = items_div[count]
 
-                # Get the name
-                item_name = item_div.find('a')['title']
-                mdcomputers_products_data[count]['item_name'] = item_name.strip()
+            # Get the name
+            item_name = item_div.find('a')['title']
+            mdcomputers_products_data[count]['item_name'] = item_name.strip()
 
-                # Get the rating
+            # Get the rating
 
-                # First we check if the product has 0 reviews, if no reviews -> rating = Unavailable
-                item_rating = item_div.find('a', {'class': 'rating-num'}).get_text()
+            # First we check if the product has 0 reviews, if no reviews -> rating = Unavailable
+            item_rating = item_div.find('a', {'class': 'rating-num'}).get_text()
 
-                if '0 reviews' in item_rating:
-                    mdcomputers_products_data[count]['item_rating'] = 'Unavailable'
+            if '0 reviews' in item_rating:
+                mdcomputers_products_data[count]['item_rating'] = 'Unavailable'
 
-                else:
-                    # Rating is present
-                    # For rating we find the number of stars that are colored out of 5 stars
-                    colored_stars = item_div.find_all('i', {'class': 'fa fa-star fa-stack-1x'})
-                    item_rating = len(colored_stars)
+            else:
+                # Rating is present
+                # For rating we find the number of stars that are colored out of 5 stars
+                colored_stars = item_div.find_all('i', {'class': 'fa fa-star fa-stack-1x'})
+                item_rating = len(colored_stars)
 
-                    mdcomputers_products_data[count]['item_rating'] = "".join([str(item_rating), '/5'])
+                mdcomputers_products_data[count]['item_rating'] = "".join([str(item_rating), '/5'])
 
-                # Get the price
-                item_price = item_div.find('span', {'class': 'price-new'})
-                # Remove the rupees symbol at the beginning
-                # Get rid of commas in the string
-                item_price = item_price.get_text().strip()[1:].replace(',', '')
-                mdcomputers_products_data[count]['item_price'] = item_price
+            # Get the price
+            item_price = item_div.find('span', {'class': 'price-new'})
+            # Remove the rupees symbol at the beginning
+            # Get rid of commas in the string
+            item_price = item_price.get_text().strip()[1:].replace(',', '')
+            mdcomputers_products_data[count]['item_price'] = item_price
 
-                # Get the link
-                item_link = item_div.find('a')['href']
-                mdcomputers_products_data[count]['item_link'] = item_link.strip()
+            # Get the link
+            item_link = item_div.find('a')['href']
+            mdcomputers_products_data[count]['item_link'] = item_link.strip()
 
-            return mdcomputers_products_data
+        return mdcomputers_products_data
 
-        # except Exception as e:
-        #     # Could not fetch data from MDComputers
-        #     return -1
+    # except Exception as e:
+    #     # Could not fetch data from MDComputers
+    #     return -1
 
 
 '''
@@ -297,6 +297,6 @@ print("Total Time taken to complete scraping", time.time() - tt)
 '''
 
 t1 = time.time()
-scraper = Scraper('gaming laptop')
+scraper = Scraper('make up')
 scraper.search()
 print("Total Time taken to complete scraping test = {}".format(time.time() - t1))

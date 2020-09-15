0eb4affc38e9f80b0cce4c1cee3f9443687127f6
premnagdeo <premnagdeo@gmail.com>
2020-09-16 03:08:45 +0530

    AJAX Request Testing Complete

---
+++ app.py
@@ -0,0 +1,17 @@
+from scraper import Scraper
+from flask import Flask, render_template, jsonify
+
+
+app = Flask(__name__)
+
+@app.route("/check_connection", methods=['GET', 'POST'])
+def check_connection():
+    return jsonify({'status': 'OK'})
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function(){
+  console.log("DEBUG script called");
+  $("#search_button").on("click", function(e) {
+    e.preventDefault();
+    console.log("DEBUG button click");
+    get_data();
+  });
+
+  $("#search_bar").keypress(function (e) {
+   var key = e.which;
+   if(key == 13)  // the enter key code
+    {
+      e.preventDefault();
+      console.log("DEBUG enter pressed");
+      get_data();
+      $('input[name = butAssignProd]').click();
+      return false;
+    }
+  });
+
+  function get_data(){
+              console.log("DEBUG get_data function called");
+                var data = $('form').serializeArray();
+                console.log(data);
+                $.ajax({
+                url: '/check_connection',
+                type: 'POST',
+                success: function(response){
+                console.log(response);
+                alert("d1");
+                },
+                error: function(error){
+                console.log(error);
+                alert("d2");
+                }
+                });
+
+  }
+
+});
